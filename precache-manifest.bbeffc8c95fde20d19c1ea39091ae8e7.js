5e176f90ce9d9a38f2c870801363fbde8fc1d4e3
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-10-07 12:52:46 +0800

    Updates

---
+++ precache-manifest.bbeffc8c95fde20d19c1ea39091ae8e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1151a1deff0976ea1e0b9119d09f3e4",
+    "url": "/weather-web/index.html"
+  },
+  {
+    "revision": "2f6fc3a0741d8f9a9c68",
+    "url": "/weather-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f5e84856f9217dfe7ce",
+    "url": "/weather-web/static/js/2.4b9073cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-web/static/js/2.4b9073cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6fc3a0741d8f9a9c68",
+    "url": "/weather-web/static/js/main.06643581.chunk.js"
+  },
+  {
+    "revision": "6aa96cdda3911498bd78",
+    "url": "/weather-web/static/js/runtime-main.22014d3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
